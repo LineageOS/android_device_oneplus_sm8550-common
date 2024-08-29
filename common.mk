1368b039cbb871c7057b0e35801c63a57a9acf2a
Bruno Martins <bgcngm@gmail.com>
2024-08-29 23:11:38 +0100

    sm8550-common: Enable AntiFlicker LiveDisplay feature
    Change-Id: I9d7da0d7b1551045f190391105164c9ebedbb683

--- common.mk
+++ common.mk
@@ -291,6 +291,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service.oplus
 
+$(call soong_config_set,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_AF,true)
+
 # Media
 PRODUCT_COPY_FILES += \
     $(AUDIO_HAL_DIR)/configs/common/codec2/media_codecs_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2_audio.xml \
